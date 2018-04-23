5f61fd3ff120aeefa60736cfc9cec8615483f42c
Alan Shaw <alan@tableflip.io>
2018-04-23 11:50:44 +0100

    allow non-repl use

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ const Commands = require('./commands')
 
 Inquirer.registerPrompt('command', InquirerCommandPrompt)
 
-module.exports = async function () {
+module.exports = async function (argv = process.argv) {
+  argv = argv.slice(2)
+  let ctx = await getInitialCtx()
+
+  if (argv.length === 0) return repl(ctx)
+
+  print(await evaluate(argv[0], ctx, argv.slice(1)))
+}
+
+async function repl (ctx) {
   console.log('\nWelcome to the IPLD explorer REPL!')
   console.log('Type "help" then <Enter> for a list of commands\n')
-
-  let ctx = await getInitialCtx()
 
   loop(async function rep () {
     ctx.autoComplete = await getAutoCompleteList(ctx)
@@ -25,62 +32,72 @@ module.exports = async function () {
       autoCompletion: s => s.includes(' ') ? ctx.autoComplete : Object.keys(Commands)
     }])
 
-    let [ cmd, ...argv ] = input.split(' ').filter(Boolean)
+    let [ cmd, ...args ] = input.split(' ').filter(Boolean)
 
-    debug(cmd, argv)
-
-    if (isIpfs.cid(cmd)) {
-      argv = [`/ipfs/${cmd}`]
-      cmd = 'cd'
-    } else if (isIpfs.path(cmd)) {
-      argv = [cmd]
-      cmd = 'cd'
-    }
-
-    if (!Commands[cmd]) return console.error(`${cmd}: command not found`)
-
-    ctx.spinner = ora().start()
-    let res
-
-    try {
-      res = await Commands[cmd](ctx, ...argv)
-    } catch (err) {
-      debug(err)
-      return ctx.spinner.fail(err.message)
-    }
-
-    ctx.spinner.stop()
+    debug(cmd, args)
+    const res = await evaluate(cmd, ctx, args)
 
     if (res && res.ctx) Object.assign(ctx, res.ctx)
-    if (res && res.out) console.log(res.out)
+    print(res)
   })
+}
 
-  async function getInitialCtx () {
-    const ipfs = IpfsApi()
-    const wd = await Commands.cd.getInitialPath(ipfs)
-    return { ipfs, wd }
+async function getInitialCtx () {
+  const ipfs = IpfsApi()
+  const wd = await Commands.cd.getInitialPath(ipfs)
+  return { ipfs, wd }
+}
+
+async function getAutoCompleteList ({ ipfs, wd }) {
+  const cmdNames = Object.keys(Commands)
+  const { value } = await ipfs.dag.get(wd)
+  let autoCompleteLinks = []
+
+  if (DAGNode.isDAGNode(value)) {
+    autoCompleteLinks = value.links.reduce((ac, l) => {
+      if (!l.name) return ac
+      return ac.concat(cmdNames.map(n => `${n} ${l.name}`))
+    }, [])
+  } else if (typeof value === 'object') {
+    autoCompleteLinks = Object.keys(value).reduce((ac, key) => {
+      return ac.concat(cmdNames.map(n => `${n} ${key}`))
+    }, [])
   }
 
-  async function getAutoCompleteList ({ ipfs, wd }) {
-    const cmdNames = Object.keys(Commands)
-    const { value } = await ipfs.dag.get(wd)
-    let autoCompleteLinks = []
+  return cmdNames.concat(autoCompleteLinks)
+}
 
-    if (DAGNode.isDAGNode(value)) {
-      autoCompleteLinks = value.links.reduce((ac, l) => {
-        if (!l.name) return ac
-        return ac.concat(cmdNames.map(n => `${n} ${l.name}`))
-      }, [])
-    } else if (typeof value === 'object') {
-      autoCompleteLinks = Object.keys(value).reduce((ac, key) => {
-        return ac.concat(cmdNames.map(n => `${n} ${key}`))
-      }, [])
-    }
+async function evaluate (cmd, ctx, args) {
+  debug(cmd, args)
 
-    return cmdNames.concat(autoCompleteLinks)
+  if (isIpfs.cid(cmd)) {
+    args = [`/ipfs/${cmd}`]
+    cmd = 'cd'
+  } else if (isIpfs.path(cmd)) {
+    args = [cmd]
+    cmd = 'cd'
   }
 
-  async function loop (func) {
-    while (true) { await func() }
+  if (!Commands[cmd]) return console.error(`${cmd}: command not found`)
+
+  ctx.spinner = ora().start()
+  let res
+
+  try {
+    res = await Commands[cmd](ctx, ...args)
+  } catch (err) {
+    debug(err)
+    return ctx.spinner.fail(err.message)
   }
+
+  ctx.spinner.stop()
+  return res
+}
+
+function print (res) {
+  if (res && res.out) console.log(res.out)
+}
+
+async function loop (func) {
+  while (true) { await func() }
 }
