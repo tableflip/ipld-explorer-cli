499698f4020fbe0acd0b6ad21b74758d124cbbc7
Alan Shaw <alan@tableflip.io>
2018-04-24 11:14:01 +0100

    adds tests for config command

---
+++ test/commands/config.test.js
@@ -0,0 +1,24 @@
+import test from 'ava'
+import config from '../../src/commands/config'
+
+test.beforeEach(t => {
+  t.context.ipfs = {}
+})
+
+test('should set apiUrl and create new IPFS instance with passed multiaddr', async t => {
+  const res = config({ ipfs: t.context.ipfs }, 'set', 'apiUrl', '/ip4/127.0.0.1/tcp/5001')
+  t.truthy(res.ctx.ipfs) // ensure changed
+  t.not(res.ctx.ipfs, t.context.ipfs) // ensure different
+})
+
+test('should error for invalid subcommand', async t => {
+  const subCmd = 'blarp'
+  const err = await t.throws(() => config({ ipfs: t.context.ipfs }, subCmd))
+  t.is(err.message, `${subCmd}: subcommand not found`)
+})
+
+test('should error for invalid key', async t => {
+  const key = 'apiURL'
+  const err = await t.throws(() => config({ ipfs: t.context.ipfs }, 'set', key))
+  t.is(err.message, `${key}: invalid config key`)
+})
